9e1d78e4777b7bb55a7f39b6848072d3b1c58548
Dror Ben-Gai <dror@snyk.io>
2017-10-18 14:47:36 +0300

    feat: supporting args as an array

---
+++ test/python-plugin.test.js
@@ -0,0 +1,26 @@
+var test = require('tap').test;
+var plugin = require('../lib').__tests;
+
+test('check build args with array', function (t) {
+  var result = plugin.buildArgs('requirements.txt', [
+    '-argOne',
+    '-argTwo',
+  ]);
+  t.match(result[0], /.*\/plug\/pip_resolve\.py/);
+  t.deepEqual(result.slice(1), [
+    'requirements.txt',
+    '-argOne',
+    '-argTwo',
+  ]);
+  t.end();
+});
+
+test('check build args with string', function (t) {
+  var result = plugin.buildArgs('requirements.txt', '-argOne -argTwo');
+  t.match(result[0], /.*\/plug\/pip_resolve\.py/);
+  t.deepEqual(result.slice(1), [
+    'requirements.txt',
+    '-argOne -argTwo',
+  ]);
+  t.end();
+});
