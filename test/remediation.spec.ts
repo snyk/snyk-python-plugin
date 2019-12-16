61cfb5832cc6e6ccb84ad5ce6bc9e6ec5270a547
Rob Cresswell <robcresswell@users.noreply.github.com>
2019-12-16 15:01:23 +0000

    feat: improve-remediation
    This patch is a rewrite of the remediation implementation. It makes
    several changes:
    
    - Remove the reliance on `inspect`, and instead get top level deps by
      parsing the manifest file. This should be significantly quicker than a
      pip install, making the fix process much faster for larger projects
    - Change the parser to attempt to better preserve package casing,
      version comparators, and extras (comments, markers, etc)
    - Update tests and add new tests. In a couple of cases, this actually
      involves fixing the test fixture, as the fixture itself was making
      changes such as package name casing, adding extra new lines to the
      manifest etc.

--- test/remediation.spec.ts
+++ test/remediation.spec.ts
@@ -1,117 +1,196 @@
-import { readdirSync, readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 import * as path from 'path';
 import { applyRemediationToManifests } from '../lib';
-import { chdirWorkspaces, deactivateVirtualenv } from './test-utils';
-import { activateVirtualenv, pipInstall } from './test-utils';
-
-function readDirAsFiles(dir: string) {
-  return readdirSync(dir).reduce(
-    (files: { [fileName: string]: string }, fileName) => {
-      files[fileName] = readFileSync(path.join(dir, fileName), 'utf8');
-      return files;
-    },
-    {}
-  );
-}
 
 describe('remediation', () => {
-  beforeEach(() => {
-    activateVirtualenv('remediation');
-  });
-
-  afterEach(() => {
-    deactivateVirtualenv();
-  });
-
-  it('fixes a pip app', async () => {
-    chdirWorkspaces('pip-app');
-    pipInstall();
-
+  it('does not add extra new lines', () => {
     const upgrades = {
-      'Django@1.6.1': { upgradeTo: 'Django@2.0.1' },
+      'django@1.6.1': { upgradeTo: 'django@2.0.1' },
       'transitive@1.0.0': { upgradeTo: 'transitive@1.1.1' },
     };
 
     const manifests = {
-      'requirements.txt': readFileSync('requirements.txt', 'utf8'),
+      'requirements.txt': 'Django==1.6.1',
     };
 
-    const expectedUpdatedManifests = readDirAsFiles(
-      '../../fixtures/updated-manifest'
-    );
+    const expectedManifests = {
+      'requirements.txt':
+        'Django==2.0.1\ntransitive>=1.1.1 # not directly required, pinned by Snyk to avoid a vulnerability',
+    };
 
-    const result = await applyRemediationToManifests(
-      '.',
-      manifests,
-      upgrades,
-      {}
-    );
+    const result = applyRemediationToManifests(manifests, upgrades);
 
-    expect(result).toEqual(expectedUpdatedManifests);
+    // Note no extra newline was added to the expected manifest
+    expect(result).toEqual(expectedManifests);
   });
 
-  it('retains python markers', async () => {
-    chdirWorkspaces('pip-app-with-python-markers');
-    pipInstall();
+  it('ignores casing in upgrades (treats all as lowercase)', () => {
+    const upgrades = {
+      'Django@1.6.1': { upgradeTo: 'Django@2.0.1' },
+    };
 
+    const manifests = {
+      'requirements.txt': 'django==1.6.1\n',
+    };
+
+    const expectedManifests = {
+      'requirements.txt': 'django==2.0.1\n',
+    };
+
+    const result = applyRemediationToManifests(manifests, upgrades);
+
+    expect(result).toEqual(expectedManifests);
+  });
+
+  it('maintains package name casing when upgrading', () => {
+    const upgrades = {
+      'django@1.6.1': { upgradeTo: 'django@2.0.1' },
+    };
+
+    const manifests = {
+      'requirements.txt': 'Django==1.6.1\n',
+    };
+
+    const expectedManifests = {
+      'requirements.txt': 'Django==2.0.1\n',
+    };
+
+    const result = applyRemediationToManifests(manifests, upgrades);
+
+    expect(result).toEqual(expectedManifests);
+  });
+
+  it('maintains comments when upgrading', () => {
+    const upgrades = {
+      'django@1.6.1': { upgradeTo: 'django@2.0.1' },
+    };
+
+    const manifests = {
+      'requirements.txt': 'django==1.6.1 # this is a comment\n',
+    };
+
+    const expectedManifests = {
+      'requirements.txt': 'django==2.0.1 # this is a comment\n',
+    };
+
+    const result = applyRemediationToManifests(manifests, upgrades);
+
+    expect(result).toEqual(expectedManifests);
+  });
+
+  it('maintains version comparator when upgrading', () => {
+    const upgrades = {
+      'django@1.6.1': { upgradeTo: 'django@2.0.1' },
+      'click@7.0': { upgradeTo: 'click@7.1' },
+    };
+
+    const manifests = {
+      'requirements.txt': 'django>=1.6.1\nclick>7.0',
+    };
+
+    const expectedManifests = {
+      'requirements.txt': 'django>=2.0.1\nclick>7.1',
+    };
+
+    const result = applyRemediationToManifests(manifests, upgrades);
+
+    expect(result).toEqual(expectedManifests);
+  });
+
+  it('fixes a pip app', () => {
+    const upgrades = {
+      'django@1.6.1': { upgradeTo: 'django@2.0.1' },
+      'transitive@1.0.0': { upgradeTo: 'transitive@1.1.1' },
+    };
+
+    const manifests = {
+      'requirements.txt': readFileSync(
+        path.resolve('test', 'workspaces', 'pip-app', 'requirements.txt'),
+        'utf8'
+      ),
+    };
+
+    const expectedManifests = {
+      'requirements.txt': readFileSync(
+        path.resolve(
+          'test',
+          'fixtures',
+          'updated-manifest',
+          'requirements.txt'
+        ),
+        'utf8'
+      ),
+    };
+
+    const result = applyRemediationToManifests(manifests, upgrades);
+
+    expect(result).toEqual(expectedManifests);
+  });
+
+  it('retains python markers', () => {
     const upgrades = {
       'click@7.0': { upgradeTo: 'click@7.1' },
     };
 
     const manifests = {
-      'requirements.txt': readFileSync('requirements.txt', 'utf8'),
+      'requirements.txt': readFileSync(
+        path.resolve(
+          'test',
+          'workspaces',
+          'pip-app-with-python-markers',
+          'requirements.txt'
+        ),
+        'utf8'
+      ),
     };
 
-    const expectedUpdatedManifests = readDirAsFiles(
-      '../../fixtures/updated-manifests-with-python-markers'
-    );
+    const expectedManifests = {
+      'requirements.txt': readFileSync(
+        path.resolve(
+          'test',
+          'fixtures',
+          'updated-manifests-with-python-markers',
+          'requirements.txt'
+        ),
+        'utf8'
+      ),
+    };
 
-    const result = await applyRemediationToManifests(
-      '.',
-      manifests,
-      upgrades,
-      {}
-    );
+    const result = applyRemediationToManifests(manifests, upgrades);
 
-    expect(result).toEqual(expectedUpdatedManifests);
+    expect(result).toEqual(expectedManifests);
   });
 
-  it('handles no-op upgrades', async () => {
-    chdirWorkspaces('pip-app');
-    pipInstall();
-
+  it('handles no-op upgrades', () => {
     const upgrades = {};
 
     const manifests = {
-      'requirements.txt': readFileSync('requirements.txt', 'utf8'),
+      'requirements.txt': readFileSync(
+        path.resolve('test', 'workspaces', 'pip-app', 'requirements.txt'),
+        'utf8'
+      ),
     };
 
-    const result = await applyRemediationToManifests(
-      '.',
-      manifests,
-      upgrades,
-      {}
-    );
+    const result = applyRemediationToManifests(manifests, upgrades);
 
     expect(result).toEqual(manifests);
   });
 
-  it('cannot fix a Pipfile app', async () => {
-    chdirWorkspaces('pipfile-pipapp');
-
+  it('cannot fix a Pipfile app', () => {
     const upgrades = {
       'Django@1.6.1': { upgradeTo: 'Django@2.0.1' },
       'transitive@1.0.0': { upgradeTo: 'transitive@1.1.1' },
     };
 
     const manifests = {
-      Pipfile: readFileSync('Pipfile', 'utf8'),
+      Pipfile: readFileSync(
+        path.resolve('test', 'workspaces', 'pipfile-pipapp', 'Pipfile'),
+        'utf8'
+      ),
     };
 
-    await expect(
-      applyRemediationToManifests('.', manifests, upgrades, {})
-    ).rejects.toMatchObject({
-      message: 'Remediation only supported for requirements.txt file',
-    });
+    expect(() => applyRemediationToManifests(manifests, upgrades)).toThrow(
+      'Remediation only supported for requirements.txt file'
+    );
   });
 });
